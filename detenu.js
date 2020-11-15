1f32f33e5f39537fe19ba48cbec928bcf6a66f72
Chloe Sauques <chloe.sauques@gmail.com>
2020-11-15 19:18:08 +0100

    ajout du CRUD de detenu et tese

--- detenu.js
+++ detenu.js
@@ -1,7 +1,6 @@
 
-//read
 module.exports = {
-    findDetenu : function(db, callback) {
+    readAllDetenu : function(db, callback) {
           // Get the documents collection
           const collection = db.collection('Detenu');
           // Find some documents
@@ -10,6 +9,49 @@ module.exports = {
                 console.log(docs)
                 callback(docs);
           });
-    }
+    },
 
+    readDetenu : function(db, callback) {
+          // Get the documents collection
+          const collection = db.collection('Detenu');
+          // Find some documents
+          collection.find({'a': 3 }).toArray(function(err, docs) {
+                console.log("Found the following records");
+                console.log(docs);
+                callback(docs);
+          });
+    },
+
+    createDetenu : function(db, callback) {
+        // Get the documents collection
+        const collection = db.collection('Detenu');
+
+        collection.insertMany([
+            {_id : 2}
+        ], function(err, result){
+            console.log("Inserted document into the collection");
+            callback(result);
+        });
+    },
+
+    updateDetenu : function(db, callback) {
+        // Get the documents collection
+        const collection = db.collection('Detenu');
+
+        collection.updateOne({_id : 2}
+            , {$set: {a : 10} }, function(err, result){
+            console.log("Update document into the collection");
+            callback(result);
+        });
+    },
+
+    deleteDetenu : function(db, callback) {
+        // Get the documents collection
+        const collection = db.collection('Detenu');
+
+        collection.deleteOne({_id : 2}, function(err, result){
+            console.log("Delete document into the collection");
+            callback(result);
+        });
+    }
 }
